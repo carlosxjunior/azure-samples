3eb7e4987c8658e6fc293953a4565a522b34b16c
Carlos Junior <carlos.junior3@ab-inbev.com>
2025-01-31 12:59:23 -0300

    add new function to test

--- Function-Apps/func-secured-storage/function_app.py
+++ Function-Apps/func-secured-storage/function_app.py
@@ -7,6 +7,28 @@ azure_blob_client = AzureBlobClient(account_name="adlsfuncdemo001")
 
 app = func.FunctionApp()
 
+@app.route(route="http_example", auth_level=func.AuthLevel.ANONYMOUS)
+def HttpExample(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
+
+"""
 @app.route("read_blob")
 def read_blob(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request to read a blob.')
@@ -53,3 +75,4 @@ def upload_blob(req: func.HttpRequest) -> func.HttpResponse:
     except Exception as e:
         logging.error(f"Error uploading blob: {e}")
         return func.HttpResponse(f"Error uploading blob: {e}", status_code=500)
+"""
