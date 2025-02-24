ba33017f73481a87c11921b35c6813ee559349cf
Carlos Junior <junior83225@gmail.com>
2025-02-24 07:33:17 -0300

    feat: add initial Azure Functions setup with Docker support and configuration files

---
+++ Azure-Pipelines/demo-deploy-container-app/src/function_app.py
@@ -0,0 +1,15 @@
+import azure.functions as func
+import datetime
+import json
+import logging
+
+app = func.FunctionApp()
+
+@app.function_name("HelloWorld")
+@app.route(route="hello-world", methods=[func.HttpMethod.POST], auth_level=func.AuthLevel.FUNCTION)
+def hello_world(req: func.HttpRequest, context=func.Context):
+    logging.info(f"PYLOG: Python http trigger function {context.function_name} processed a request.")
+    return func.HttpResponse(
+        "Hello, World",
+        status_code=200
+    )
