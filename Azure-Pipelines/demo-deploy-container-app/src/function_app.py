f8ec2edaa82a37d895f4de04ee49be0f0916804e
Carlos Junior <junior83225@gmail.com>
2025-02-24 07:43:30 -0300

    fix: change auth level of HelloWorld function to ANONYMOUS

--- Azure-Pipelines/demo-deploy-container-app/src/function_app.py
+++ Azure-Pipelines/demo-deploy-container-app/src/function_app.py
@@ -6,7 +6,7 @@ import logging
 app = func.FunctionApp()
 
 @app.function_name("HelloWorld")
-@app.route(route="hello-world", methods=[func.HttpMethod.POST], auth_level=func.AuthLevel.FUNCTION)
+@app.route(route="hello-world", methods=[func.HttpMethod.POST], auth_level=func.AuthLevel.ANONYMOUS)
 def hello_world(req: func.HttpRequest, context=func.Context):
     logging.info(f"PYLOG: Python http trigger function {context.function_name} processed a request.")
     return func.HttpResponse(
